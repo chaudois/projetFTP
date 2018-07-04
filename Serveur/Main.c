ddc804ee4be8679d271e3ab4536ff65506929342
chaudois <damien.chaudois@gmail.com>
2018-07-04 11:09:44 +0200

    première version du serveur "viable"

--- Serveur/Main.c
+++ Serveur/Main.c
@@ -4,8 +4,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h> 
-#include <netdb.h>  
+#include <unistd.h>
+#include <netdb.h>
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
 #define closesocket(s) close(s)
@@ -22,46 +22,43 @@ int main()
 		perror("socket()");
 		exit(-1);
 	}
-	else
+	printf("Socket créé : %d\n", sock);
+	SOCKADDR_IN sin = {0};
+
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	sin.sin_family = AF_INET;
+
+	sin.sin_port = htons(999);
+
+	if (bind(sock, (SOCKADDR *)&sin, sizeof sin) == SOCKET_ERROR)
 	{
-		printf("Socket créé : %d\n", sock);
-		SOCKADDR_IN sin = {0};
-
-		sin.sin_addr.s_addr = htonl(INADDR_ANY);
-
-		sin.sin_family = AF_INET;
-
-		sin.sin_port = htons(999);
-
-		if (bind(sock, (SOCKADDR *)&sin, sizeof sin) == SOCKET_ERROR)
-		{
-			perror("bind()");
-			exit(-1);
-		}
-		else
-		{
-			if (listen(sock, 5) == SOCKET_ERROR)
-			{
-				perror("listen()");
-				exit(-1);
-			}
-			else
-			{
-				SOCKADDR_IN csin = {0};
-				SOCKET csock;
-
-				int sinsize = sizeof csin;
-
-				csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
-
-				if (csock == INVALID_SOCKET)
-				{
-					perror("accept()");
-					exit(-1);
-				}
-			}
-		}
+		perror("bind()");
+		exit(-1);
 	}
+	if (listen(sock, 5) == SOCKET_ERROR)
+	{
+		perror("listen()");
+		exit(-1);
+	}
+	SOCKADDR_IN csin = {0};
+	SOCKET csock;
+
+	int sinsize = sizeof csin;
+	int continuer = 1;
+	while (continuer)
+	{
+
+		csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
+
+		if (csock == INVALID_SOCKET)
+		{
+			perror("accept()");
+		}
+		printf("connexion entrante : socket n° %d\n", csock);
+	}
+	closesocket(sock);
+	closesocket(csock);
 
 	return 0;
 }
